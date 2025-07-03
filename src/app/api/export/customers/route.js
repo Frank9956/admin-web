4c11836b1a9326294846805d428345cc68213921
Tilak Singh <113658838+Frank9956@users.noreply.github.com>
2025-07-03 06:32:39 +0530

    new

--- src/app/api/export/customers/route.js
+++ src/app/api/export/customers/route.js
@@ -1,14 +1,22 @@
 import { google } from 'googleapis';
 import { getDocs, collection } from 'firebase/firestore';
 import { db } from '@/lib/firebase/firebase';
-import path from 'path';
-import { promises as fs } from 'fs';
 
 export async function GET() {
   try {
-    const serviceAccountPath = path.join(process.cwd(), 'habit-us-c24d0-6b4f9b73510e.json');
-    const keyFile = await fs.readFile(serviceAccountPath, 'utf-8');
-    const credentials = JSON.parse(keyFile);
+    const credentials = {
+      type: process.env.GOOGLE_TYPE,
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      auth_uri: process.env.GOOGLE_AUTH_URI,
+      token_uri: process.env.GOOGLE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+      universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+    };
 
     const auth = new google.auth.GoogleAuth({
       credentials,
--- src/app/api/export/orders/route.js
+++ src/app/api/export/orders/route.js
@@ -8,9 +8,19 @@ import { promises as fs } from 'fs';
 
 export async function GET() {
   try {
-    const serviceAccountPath = path.join(process.cwd(), 'habit-us-c24d0-6b4f9b73510e.json');
-    const keyFile = await fs.readFile(serviceAccountPath, 'utf-8');
-    const credentials = JSON.parse(keyFile);
+    const credentials = {
+      type: process.env.GOOGLE_TYPE,
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.split(String.raw`\n`).join('\n'),
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      auth_uri: process.env.GOOGLE_AUTH_URI,
+      token_uri: process.env.GOOGLE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+      universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+    };
 
     const auth = new google.auth.GoogleAuth({
       credentials,
@@ -37,7 +47,7 @@ export async function GET() {
 
     await sheets.spreadsheets.values.update({
       spreadsheetId: sheetId,
-      range: 'Sheet1!A1',
+      range: 'Orders!A1', 
       valueInputOption: 'RAW',
       requestBody: { values },
     });
